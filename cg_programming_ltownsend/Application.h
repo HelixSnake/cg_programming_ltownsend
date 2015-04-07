530aa25aacf65c86cb6df6192a8e0a5021572059
unknown <r202-stu@AI241LAB0202P18.aipdcls.edu>
2015-04-06 20:33:09 -0700

    first commit

---
+++ cg_programming_ltownsend/Application.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// Standard headers...
+#include <stdio.h>
+#include <stdlib.h>
+#include <cassert>
+#include <iostream>
+#include <stdexcept>
+#include <cmath>
+
+// Namespaces...
+using namespace std;
---
+++ cg_programming_ltownsend/Main.cpp
@@ -0,0 +1,11 @@
+#include "Application.h"
+
+int main() {
+	
+	cout << "sup bicthes" << endl;
+	
+	// Game Loop...
+	do{
+
+	}while(true);
+}
