060bbdf4466d3c8f0c2344d5057de2442cfb3b0c
unknown <r202-stu@AI241LAB0202P18.aipdcls.edu>
2015-04-13 21:46:20 -0700

    Added framework code

--- cg_programming_ltownsend/Application.h
+++ cg_programming_ltownsend/Application.h
@@ -1,13 +1,38 @@
 #pragma once
 
+//GLEW provides the function pointers we need t
+#include <GL/glew.h>
+
+#include <glfw3.h>
+
+#include <glm/glm.hpp>
+
 // Standard headers...
 #include <stdio.h>
+#include <memory.h>
 #include <stdlib.h>
 #include <cassert>
 #include <iostream>
 #include <stdexcept>
 #include <cmath>
-#include <glew.h>
 
 // Namespaces...
+using namespace glm; // For OpenGL math
 using namespace std;
+
+// Defines...
+#define GLFW_DLL
+#define APP_NAME "Landon Townsend's Kickass OpenGL Project"
+#define EXIT_WITH_ERROR -1
+#define EXIT_WITH_SUCCESS 0
+#define OPEN_GL_VERSION 3 // Specifies OpenGL 3.3
+#define ANTIALIASING 4 // nx antialiasing
+#define SCREEN_WIDTH 1024
+#define SCREEN_HEIGHT 768
+
+#define GLFW_FAIL 0
+
+// External Globals...
+extern GLFWwindow* window;
+extern GLint shaderStatus;
+extern bool activeRef;
--- cg_programming_ltownsend/Main.cpp
+++ cg_programming_ltownsend/Main.cpp
@@ -1,11 +1,59 @@
 #include "Application.h"
 
+GLFWwindow* window = NULL;
+
+int InitWindowFailed() {
+	if(glfwInit() == GLFW_FAIL) {
+		fprintf(stderr, "Failed to initialize GLFW/n");
+		return EXIT_WITH_ERROR;
+	}
+	glfwWindowHint(GLFW_SAMPLES, ANTIALIASING);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, OPEN_GL_VERSION);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, OPEN_GL_VERSION);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Mac compatibility
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Use new OpenGL version
+
+	window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, APP_NAME, NULL, NULL);
+
+	if (window == NULL) {
+		fprintf(stderr, "Failed to create/open GLFW window/n");
+		glfwTerminate();
+		return EXIT_WITH_ERROR;
+	}
+
+	//Initialize GLEW.
+	glfwMakeContextCurrent(window);
+
+	//Ensure we can capture the escape key being pressed below
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	return EXIT_WITH_SUCCESS;
+}
+
+int InitGlewFailed() {
+	glewExperimental = true; // has to do with core profiling
+	if(glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW/n");
+		return EXIT_WITH_ERROR;
+	}
+
+	return EXIT_WITH_SUCCESS;
+}
+
 int main() {
-	
-	cout << "sup bicthes" << endl;
-	
+	if (InitWindowFailed() | InitGlewFailed()) {
+		system("PAUSE");
+		return EXIT_WITH_ERROR;
+	}
+
 	// Game Loop...
 	do{
+		//Update();
+		//Render();
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	} while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+		glfwWindowShouldClose(window) == 0);
 
-	}while(true);
+	return EXIT_WITH_SUCCESS;
 }
